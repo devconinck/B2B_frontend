fa38b1f5714e1d89b8e3be4241582865cb408cef
Quinten <quinten.deconinck@student.hogent.be>
2024-04-15 23:46:22 +0200

    added ShadcnUI + old layout

---
+++ sdp02/components/Header.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import Container from "./ui/container";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu, Moon, ShoppingCart, Sun } from "lucide-react";
+
+import ModeToggle from "./ModeToggle";
+
+const Header = () => {
+  const routes = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/contact",
+      label: "Contact",
+    },
+  ];
+
+  return (
+    <header className="border-b px-4 py-3 sm:flex sm:justify-between">
+      <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      <Container>
+        <div className="relative flex h-16 w-full items-center justify-between px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center">
+            <Sheet>
+              <SheetTrigger>
+                <Menu className="h-6 w-6 md:hidden" />
+              </SheetTrigger>
+              <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+                <nav className="flex flex-col gap-4">
+                  {routes.map((route, i) => (
+                    <Link
+                      key={i}
+                      href={route.href}
+                      className="block px-2 py-1 text-lg"
+                    >
+                      {route.label}
+                    </Link>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
+            <Link href="/" className="ml-4 lg:ml-0">
+              <h1 className="text-xl font-bold">Frituur d`Aa</h1>
+            </Link>
+          </div>
+          <nav className="mx-6 flex  items-center space-x-4 md:block lg:space-x-6">
+            {routes.map((route, i) => (
+              <Button key={i} asChild variant="ghost">
+                <Link
+                  key={i}
+                  href={route.href}
+                  className="text-sm font-medium transition-colors"
+                >
+                  {route.label}
+                </Link>
+              </Button>
+            ))}
+          </nav>
+          <div className="mr-2 flex items-center">
+            <Link href={"/order"}>
+              <Button className="mr-4">Order</Button>
+            </Link>
+            <ModeToggle />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
--- sdp02/pages/_app.tsx
+++ sdp02/pages/_app.tsx
@@ -1,6 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { type AppType } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import "@/styles/globals.css";
+
+import { ThemeProvider } from "../components/theme-provider";
+import Layout from "./layout";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
+};
+
+export default MyApp;
