4c8b9de3a8e2aef9e93b618252bfd6c70930f30c
MaximLison <maxim.lison@student.hogent.be>
2024-04-23 17:08:34 +0200

    Pagination for Ordertable finished

--- sdp02/pages/orders/ordertable.tsx
+++ sdp02/pages/orders/ordertable.tsx
@@ -31,6 +31,14 @@ import {
   getFilteredRowModel,
 } from "@tanstack/react-table";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -154,14 +162,34 @@ export const OrderTable = <TData, TValue>({
           <div className="flex-items-center space-x-2">
             <p className="text-sm font-medium">Rows per page</p>
           </div>
-          <div className="flex items-center justify-center text-sm font-medium">
-            Page 1 of 10 {/*is nog hard gecodeerd*/}
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+            Page {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
           </div>
           <div className="flex items-center space-x-2">
             <Button
               variant="outline"
               size="sm"
-              onClick={() => table.previousPage()}
+              onClick={() => {
+                table.previousPage();
+              }}
               disabled={!table.getCanPreviousPage()}
             >
               Previous
@@ -169,7 +197,9 @@ export const OrderTable = <TData, TValue>({
             <Button
               variant="outline"
               size="sm"
-              onClick={() => table.nextPage()}
+              onClick={() => {
+                table.nextPage();
+              }}
               disabled={!table.getCanNextPage()}
             >
               Next
