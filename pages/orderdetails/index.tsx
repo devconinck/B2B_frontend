334d637c4df5729c2ab3f009d902a3c116de8abf
Arno Heekhout <114562521+arno-heekhout@users.noreply.github.com>
2024-05-02 11:12:21 +0200

    Tijdelijke oplossing demo

--- pages/orderdetails/index.tsx
+++ pages/orderdetails/index.tsx
@@ -296,7 +296,7 @@ const OrderDetails: NextPage = () => {
           <button
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
             // TODO vervangen door correcte orderID
-            onClick={ () => handleDownloadInvoice(mockCustomers[0].orderid)}
+            onClick={ () => handleDownloadInvoice()}
           >
             Download Invoice
           </button>
